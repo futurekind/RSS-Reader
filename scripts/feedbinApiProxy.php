a4e6fca898bd081d2e27f7bf22ebdee4cc18cd6e
futurekind <f.trinler@gmail.com>
2013-07-12 21:54:42 +0200

    Add a proxy script for the api calls

---
+++ scripts/feedbinApiProxy.php
@@ -0,0 +1,38 @@
+<?php
+class FeedbinApiProxy {
+	var $urlSubscription = "https://api.feedbin.me/v2/subscriptions.json";
+
+	function getSubscriptions(){
+		$result['data'] = $this->curl($this->urlSubscription);
+		return $this->renderJSON($result);
+	}
+
+	private function curl($url, $options = []){
+		$ch = curl_init($url);
+
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		$result = curl_exec($ch);
+
+		$info = curl_getinfo($ch);
+
+		curl_close($ch);
+
+		if($info['http_code'] != '200'){
+			$result = $info;
+		}
+
+		return $result;
+	}
+
+	private function renderJSON($data) {
+		header('Content-Type: application/json');
+		echo json_encode($data);
+	}
+
+}
+
+$proxy = new FeedbinApiProxy();
+$proxy->$_GET['command']();
+
+?>
