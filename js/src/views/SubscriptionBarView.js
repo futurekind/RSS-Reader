86c81b0a261b968e34b28e071ad1f7b6023982c3
futurekind <f.trinler@gmail.com>
2013-07-17 22:35:47 +0200

    Add simple change event for subscription collection

--- js/src/views/SubscriptionBarView.js
+++ js/src/views/SubscriptionBarView.js
@@ -27,7 +27,9 @@ var SubscriptionBarView = Backbone.View.extend({
 			if(data.http_code == '401'){
 				new LoginFormView();
 			} else {
+
 				_this.subscriptions = new Subscriptions();
+				_this.subscriptions.on( "change", _this.render, _this);
 
 				$.each(data, function(i, s){
 					var subscription = new Subscription(s);
@@ -41,11 +43,13 @@ var SubscriptionBarView = Backbone.View.extend({
 	},
 
 	render: function() {
-		console.log(this.subscriptions);
+		// console.log(this.subscriptions);
 		var templateHtml = _.template(this.template, {data: this.subscriptions.models});
 
 		this.$el.html(templateHtml);
 
 		$('#app-feeds-bar').append(this.$el);
+
+		this.subscriptions.get(618762).set('title', 'foo');
 	}
 });
